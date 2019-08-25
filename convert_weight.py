8add4a4d1188bba3f4c38a6ff94385104dce15db
XaNcE <53494572+xancenl@users.noreply.github.com>
2019-08-25 19:54:31 +0200

    tensorflow namespace corrections
    Tensorflow code, functions and class rewrites to prevent deprecations for support of future v1 releases

--- convert_weight.py
+++ convert_weight.py
@@ -18,16 +18,16 @@ weight_path = './data/darknet_weights/yolov3.weights'
 save_path = './data/darknet_weights/yolov3.ckpt'
 anchors = parse_anchors('./data/yolo_anchors.txt')
 
-model = yolov3(80, anchors)
-with tf.Session() as sess:
-    inputs = tf.placeholder(tf.float32, [1, img_size, img_size, 3])
+model = yolov3(num_class, anchors)
+with tf.compat.v1.Session() as sess:
+    inputs = tf.compat.v1.placeholder(tf.float32, [1, img_size, img_size, 3])
 
-    with tf.variable_scope('yolov3'):
+    with tf.compat.v1.variable_scope('yolov3'):
         feature_map = model.forward(inputs)
 
-    saver = tf.train.Saver(var_list=tf.global_variables(scope='yolov3'))
+    saver = tf.compat.v1.train.Saver(var_list=tf.compat.v1.global_variables(scope='yolov3'))
 
-    load_ops = load_weights(tf.global_variables(scope='yolov3'), weight_path)
+    load_ops = load_weights(tf.compat.v1.global_variables(scope='yolov3'), weight_path)
     sess.run(load_ops)
     saver.save(sess, save_path=save_path)
     print('TensorFlow model checkpoint has been saved to {}'.format(save_path))
